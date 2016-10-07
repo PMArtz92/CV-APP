24575344260612acb28ef57028b21c774a18662b
pmartz92 <pmartz92@gmail.com>
2016-10-07 17:19:08 +0530

    cvupload conflict fixed

--- api/client/src/app/main/cvs/uploadcv/cvUpload.controller.js
+++ api/client/src/app/main/cvs/uploadcv/cvUpload.controller.js
@@ -6,9 +6,24 @@
     .controller('CVUpload', CVUpload);
 
   /** @ngInject */
-  function CVUpload($scope,$element) {
-    $scope.list = ['test', 'rrr', 'ffff'];
-    $scope.companies = ['WSO2', '99X', 'MIT', 'Cambio', 'IFS', 'Code Gen'];
+  function CVUpload($scope, $element) {
+    $scope.companies = [{
+      name: 'MIT',
+      catv: true,
+      cat: ['SE', 'BA', 'QA','PM'],
+
+    }, {
+      name: '99X',
+      catv: true,
+      cat: ['SE', 'BA', 'QA'],
+
+    }, {
+      name: 'Caklabs',
+      catv: false,
+      cat: [],
+
+    }];
+
     $scope.searchTerm;
     $scope.clearSearchTerm = function() {
       $scope.searchTerm = '';
@@ -19,9 +34,51 @@
       ev.stopPropagation();
     });
     //print($scope.ccc);
+    $scope.formData = {};
+
+    $scope.$watch('formData.company1', function() {
+      for (var i = 0; i < $scope.companies.length; i++) {
+        if ($scope.companies[i].name == $scope.formData.company1) {
+          $scope.selectedObject = $scope.companies[i];
+          if($scope.selectedObject.catv==false){
+            $scope.formData.position1="";
+          }
+          $scope.secondpre = true;
+        }
+      }
+    });
+
+    $scope.$watch('formData.company2', function() {
+      for (var i = 0; i < $scope.companies.length; i++) {
+        if ($scope.companies[i].name == $scope.formData.company2) {
+          $scope.selectedObject1 = $scope.companies[i];
+          if($scope.selectedObject1.catv==false){
+            $scope.formData.position2="";
+          }
+          $scope.thirdpre=true;
+
+        }
+      }
+    });
+    $scope.$watch('formData.company3', function() {
+      for (var i = 0; i < $scope.companies.length; i++) {
+        if ($scope.companies[i].name == $scope.formData.company3) {
+          $scope.selectedObject2 = $scope.companies[i];
+          if($scope.selectedObject2.catv==false){
+            $scope.formData.position3="";
+          }
+
+        }
+      }
+    });
+
+
+    $scope.sendForm = function() {
+
+      //console.log($scope.formData.selected.length);
+      console.log($scope.formData);
+    }
 
   }
-  function print(x){
-    console.log(x);
-  }
+
 })();
